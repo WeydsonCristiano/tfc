f66cfcc77e5bd730dae3f2d7d5dc0ec8c232dd93
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-03-03 11:34:27 -0300

    refactore: 'functions'

--- app/backend/src/interface/TeamLeaderBoard.ts
+++ app/backend/src/interface/TeamLeaderBoard.ts
@@ -5,7 +5,7 @@ export default class TeamLeaderBoard {
   private _type: 'home' | 'away';
   public name: string;
   public totalPoints: number;
-  public totalGames: number;
+  public totalGames: number | undefined;
   public totalVictories: number;
   public totalDraws: number;
   public totalLosses: number;
@@ -27,5 +27,69 @@ export default class TeamLeaderBoard {
     this.goalsOwn = 0;
     this.goalsBalance = 0;
     this.efficiency = '';
+
+    if (type === 'home') this.sumHomePoints();
+    if (type === 'away') this.sumAwayPoints();
+
+    this.efficiencyTeams();
+    this.calcGoals();
+  }
+
+  public sumHomePoints() {
+    const { homeMatches, awayMatches } = this._team;
+    const matches = this._type === 'home' ? homeMatches : awayMatches;
+
+    matches?.forEach(({ homeTeamGoals, awayTeamGoals }) => {
+      if (homeTeamGoals > awayTeamGoals) {
+        this.totalPoints += 3;
+        this.totalVictories += 1;
+      } else if (homeTeamGoals < awayTeamGoals) {
+        this.totalLosses += 1;
+      } else {
+        this.totalPoints += 1;
+        this.totalDraws += 1;
+      }
+    });
+  }
+
+  public sumAwayPoints() {
+    const { homeMatches, awayMatches } = this._team;
+    const matches = this._type === 'home' ? homeMatches : awayMatches;
+
+    matches?.forEach(({ homeTeamGoals, awayTeamGoals }) => {
+      if (awayTeamGoals > homeTeamGoals) {
+        this.totalPoints += 3;
+        this.totalVictories += 1;
+      } else if (awayTeamGoals < homeTeamGoals) {
+        this.totalLosses += 1;
+      } else {
+        this.totalPoints += 1;
+        this.totalDraws += 1;
+      }
+    });
+  }
+
+  public efficiencyTeams() {
+    const { homeMatches, awayMatches } = this._team;
+    const matches = this._type === 'home' ? homeMatches : awayMatches;
+
+    this.totalGames = matches?.length;
+
+    if (this.totalGames) {
+      const efficiency = (this.totalPoints / (this.totalGames * 3)) * 100;
+      this.efficiency = efficiency.toFixed(2);
+    }
+  }
+
+  public calcGoals() {
+    const { homeMatches, awayMatches } = this._team;
+    const matches = this._type === 'home' ? homeMatches : awayMatches;
+
+    matches?.forEach(({ homeTeamGoals, awayTeamGoals }) => {
+      this.goalsFavor += this._type === 'away' ? awayTeamGoals : homeTeamGoals;
+      this.goalsOwn += this._type === 'away' ? homeTeamGoals : awayTeamGoals;
+    });
+
+    this.goalsBalance = this.goalsFavor - this.goalsOwn;
   }
 }
