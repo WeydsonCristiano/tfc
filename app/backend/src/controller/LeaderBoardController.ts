59d93b148796a243760610d463b0a940b637e6f2
WeydsonCristiano <weydsoncristiano@hotmail.com>
2023-03-03 11:33:41 -0300

    refactore: 'controller'

--- app/backend/src/controller/LeaderBoardController.ts
+++ app/backend/src/controller/LeaderBoardController.ts
@@ -2,9 +2,8 @@ import { Request, Response } from 'express';
 import LeaderboardService from '../services/LeaderboardService';
 
 export default class LeaderboardController {
-  static async getStatusHome(req: Request, res: Response) {
-    const { id } = req.params;
-    const respHome = await LeaderboardService.getStatusHome(Number(id));
+  static async getStatusHome(_req: Request, res: Response) {
+    const respHome = await LeaderboardService.getStatusHome('home');
     res.status(200).json(respHome);
   }
 }
